501e26f96ee467e5d5ec32a6ec49f0aa20b7c521
pratikadamas <147491382+pratikadamas@users.noreply.github.com>
2024-12-15 03:03:29 +0530

    basic python opreator

---
+++ Arithmatic operator 05.py
@@ -0,0 +1,22 @@
+
+
+a=int(input("Enter the number:"))
+b=int(input("Enter the number:"))
+print("Addition of two number")
+print(a+b)
+print("Subtraction of two number")
+print(a-b)
+print("Multiplication of two number")
+print(a*b)
+print("Division of two number")
+print(a/b)
+print("Remainder or two number")
+print(a%b)
+print("Exponent of two number")
+print(a**b)
+print("Floor division of two number")
+print(a//b)
+
+
+
+
--- Lgical operator 04.py
+++ Lgical operator 04.py
@@ -3,7 +3,8 @@ A=126
 B=123
 C=A and B  # For and, Python returns the second operand (B) if both are True. Thus, C = 123.
 D=0 or 0
-#E=A not B
+E= not B
 print(C)
 print(D)
 print(1 and A>B)
+print(E)
