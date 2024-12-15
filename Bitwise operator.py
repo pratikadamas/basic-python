5325463fb29323b980c3acb40f07b3db97d03e39
pratikadamas <147491382+pratikadamas@users.noreply.github.com>
2024-12-16 00:03:50 +0530

    bitwise operator

---
+++ Bitwise operator.py
@@ -0,0 +1,10 @@
+x=int(input("Enter a number"))
+y=int(input("Enter another number"))
+
+print(" Binary of the 1st number is ",bin(x))
+print(" Binary of the 2st number is ",bin(y))
+
+print("After bitwise AND opration ",x&y)
+print("After bitwise OR opration ",x|y)
+print("After bitwise X-OR opration ",x^y)
+
