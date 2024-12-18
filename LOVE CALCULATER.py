32b9047d8ac5434f0e8b0a9d3c87696dd7f48f40
pratikadamas <147491382+pratikadamas@users.noreply.github.com>
2024-12-18 19:52:43 +0530

    LOVE CALCULATER IN PYTHON

---
+++ LOVE CALCULATER.py
@@ -0,0 +1,27 @@
+
+#----------------------LOVE CALCULATER-------------
+
+str1=input("Enter your name :",)
+str2=input("Enter your partner name :",)
+
+print(str1 , " Loves ", str2)
+
+str3=str1+str2
+
+str4=str3.lower()
+a=str4.count("t")
+b=str4.count("r")
+c=str4.count("u")
+d=str4.count("e")
+f=a+b+c+d
+
+e=str4.count("l")
+i=str4.count("o")
+g=str4.count("v")
+h=str4.count("e")
+
+j=e+i+g+h
+
+print(" Perentage of LOVE :", str(f)+str(f),"%")
+
+
