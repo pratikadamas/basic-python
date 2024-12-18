9f92e912eedc57c944ea78975314fe7d438c3011
pratikadamas <147491382+pratikadamas@users.noreply.github.com>
2024-12-18 20:00:11 +0530

    fstring in python

---
+++ fstring.py
@@ -0,0 +1,8 @@
+
+
+str=input(" Enter your name:")
+
+age=int(input("Enter your age :", ))
+height=int(input(" Enter your height :"))
+
+print(f"My name is {str} and I am {age} years old. My height is {height} cm ")
