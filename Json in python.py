0d0a496f700ce2cbdfbe204ad90ef0ef2d01f54e
pratikadamas <147491382+pratikadamas@users.noreply.github.com>
2024-12-25 12:22:44 +0530

    read & write JSON FILE

---
+++ Json in python.py
@@ -0,0 +1,10 @@
+
+
+import json
+d= {"course_name":"python","fees":1200}
+
+f=json.dumps(d)
+
+print(type(f))
+
+print(f)
---
+++ read & write JOSON file.py
@@ -0,0 +1,13 @@
+
+import json
+file=open(r"E:\basic python\json file.py","r")
+data =file.read()
+
+final_data=json.loads(data)
+
+# print(final_data)
+
+
+for a in final_data:
+    
+    print(a," : ",final_data[a])
