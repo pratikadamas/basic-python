749196ff78791f4f25d870a18193e7c6cc3bc94f
pratikadamas <147491382+pratikadamas@users.noreply.github.com>
2024-12-31 20:26:37 +0530

    List comprehension

---
+++ nested list comprehension.py
@@ -0,0 +1,13 @@
+
+
+list1=[1,2,3]
+list2=[4,5,6]
+CombLst=[(x,y) for x in list1 for y in list2]
+print (CombLst)
+
+list3 = [x for x in range(1,100) if(x%3==0)]
+
+print("Number divisible by 3 is :")
+
+print(list3)
+
