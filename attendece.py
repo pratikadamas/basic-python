2821b1bc22760eeb4ebcfc1c7cb932ad6a964a05
pratikadamas <147491382+pratikadamas@users.noreply.github.com>
2025-03-12 23:42:05 +0530

    Student attendence using set theory in python

--- attendece.py
+++ attendece.py
@@ -3,15 +3,15 @@ student_roll = set()
 present_stu_roll=set()
 
 previous_input = None
-for i in range(23,30,1):
-    student_roll.add(i)
+for i in range(123,196,1):
+    student_roll.add(str(i))
 
 
 print(student_roll)
 print("Enter present student roll")
-for i in range(23,30,1):
-    x= input()
-    if(x==0):
+for i in range(123,196,1):
+    x = input()
+    if(x=='0'):
         break
     present_stu_roll.add(x)
 
@@ -19,4 +19,6 @@ print(present_stu_roll)
 
 
 absent_stu_roll = student_roll.difference(present_stu_roll)
+print("----------------------absent student roll--------------------------")
 print(absent_stu_roll)
+
