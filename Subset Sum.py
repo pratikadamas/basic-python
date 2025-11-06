f2b451a3e874ff727fb82a720989bb3d9b713fbd
Pratik Giri <pratikgiri585@gmail.com>
2025-11-07 00:44:29 +0530

    Subset sum and all sper set does not contains duplicates

--- Subset Sum.py
+++ Subset Sum.py
@@ -1,11 +1,13 @@
-from itertools import combinations, permutations
+from itertools import combinations
 def CombinationSum(nums):
     if len(nums) == 0:
+        print("array length is zero so return ")
         return -1
     l=list()
 
-    for i in range (len(nums)):
+    for i in range (0,len(nums)+1):
         l2=list(combinations(nums,i))
+        print(l2)
         for j in l2:
             l3=list(j)
          # print(l2)
@@ -13,7 +15,26 @@ def CombinationSum(nums):
     return l
 
 
+def SupersetWithoutDuplicate(nums):
+    if len(nums) == 0:
+        print("array length is zero so return ")
+        return -1
+    l=list()
 
-ar=[1,2,3]
+    for i in range (0,len(nums)+1):
+        l2=list(combinations(nums,i))
+        print(l2)
+        for j in l2:
+            l3=list(j)
+            if l3 not in l:
+                l.append(l3)
+
+    return l
+
+
+ar=[1,2,2]
+a2=[]
 print (sum(ar))
 print(CombinationSum(ar))
+print(CombinationSum(a2))
+print(SupersetWithoutDuplicate(ar))
