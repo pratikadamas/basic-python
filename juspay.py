67514bb6e3264f712278eca733607bddf9f02b4a
Pratik Giri <pratikgiri585@gmail.com>
2026-04-01 01:27:49 +0530

    ..

--- juspay.py
+++ juspay.py
@@ -5,6 +5,7 @@ def juspay(s, k):
     if(len(s)==1):
         return -1
     perms = [''.join(p) for p in permutations(s, k)]
+    print(perms)
     perms.sort()
     print("All possible size k keys in sorted order ",perms)
 
@@ -14,4 +15,15 @@ def juspay(s, k):
     return -1
 
 
-print(juspay("abc", 2))
+def per(s):
+    all_perms = []
+    for p in range(1, len(s) + 1):
+        for perm in permutations(sorted(s), p):  # sort input first
+            all_perms.append(''.join(perm))
+
+    for perm in sorted(all_perms):
+        print(perm)
+
+
+print(juspay("cba", 2))
+print(per("cab"))
